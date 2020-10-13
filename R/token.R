90d5231d9c542554fa9a7acc2ac02ee783b5c613
Shinya Uryu <suika1127@gmail.com>
2020-10-14 08:33:01 +0900

    as tibble

--- R/token.R
+++ R/token.R
@@ -4,6 +4,7 @@
 #' @export
 #' @rdname mecab_token_df
 mecab_token_df <- function(data, sentence, sys_dic = "", user_dic = "") {
+  subtype <- NULL
   data %>%
     dplyr::rename(input_var = {{ sentence }}) %>%
     dplyr::group_modify(
@@ -11,5 +12,7 @@ mecab_token_df <- function(data, sentence, sys_dic = "", user_dic = "") {
                        format = "data.frame",
                        sys_dic = sys_dic,
                        user_dic = user_dic)) %>%
-    dplyr::ungroup()
+    dplyr::ungroup() %>%
+    dplyr::mutate(subtype = dplyr::na_if(subtype, "")) %>%
+    tibble::as_tibble()
 }
